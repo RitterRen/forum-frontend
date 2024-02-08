c38e81cd961dd373812d28cf767da75b513cee3a
wanghaox2020 <79470452+wanghaox2020@users.noreply.github.com>
2024-02-08 08:45:11 -0800

    20240208haoxiang userprofile finished (#10)
    * add submit button and logic on Contact page
    
    * user-detail edit part finished
    
    ---------
    
    Co-authored-by: Ritter Ren <90739168+RitterRen@users.noreply.github.com>

--- src/components/UserDetail/index.tsx
+++ src/components/UserDetail/index.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEvent, SyntheticEvent, useEffect } from 'react';
 import { HistoryModel, UserModel } from '../../types';
 import { useState } from 'react'
 import { Typography } from '@mui/material';
 import { showFormattedDate } from '../util';
-import { Grid, Avatar, Button, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { Grid, Avatar, List, ListItem, ListItemText, Paper, DialogContentText } from '@mui/material';
 import { EditUserProfile } from './EditUserProfile/EditUserProfile';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Box } from '@mui/material';
+// import './userProfile.css';
+// import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 const token = localStorage.getItem("token");
 
 export const UserProfile = () => {
     const [updateTrigger, setUpdateTrigger] = useState(0);
+    const [verifyCode, setVerifyCode] = useState('');
 
     const [user, setUser] = useState<UserModel>({
         userId: 0,
@@ -21,10 +25,62 @@ export const UserProfile = () => {
         role: '',
         profileImageURL: '',
     })
+    const [file, setFile] = useState<File | null>(null);
+    const [link, setLink] = useState('');
 
     const [history, setHistory] = useState<HistoryModel[]>([])
 
     const [isEditModalOpen, setEditModalOpen] = useState(false);
+
+    const [openVerifyDialog, setOpenVerifyDialog] = useState(false);
+
+    const [openFileUpload, setOpenFileUpload] = useState(false);
+
+    const onOpenFileUpload = () => setOpenFileUpload(true)
+    const onCloseFileUpload = () => setOpenFileUpload(false);
+
+    const handleOpenVerifyDialog = () => {
+        setOpenVerifyDialog(true);
+    };
+
+    const handleCloseVerifyDialog = () => {
+        setOpenVerifyDialog(false);
+    };
+
+    const handleVerify = () => {
+        // Implementation for handling the verification code
+        const fetchVerifyCode = async () => {
+            const apiEndpoint = `http://localhost:8888/api/user/email/auth/${verifyCode}`;
+            try {
+                const response = await fetch(apiEndpoint, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`, // Assuming a Bearer token
+                    },
+                });
+                // console.log(response)
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
+                }
+
+                const responseData = await response.json(); // Assuming the server responds with JSON
+                console.log(responseData); // Do something with the response data
+                setUpdateTrigger(prev => prev + 1)
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        console.log(verifyCode)
+        fetchVerifyCode();
+        handleCloseVerifyDialog();
+    };
+
+    const handlerVerifyCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target
+        setVerifyCode(value)
+    }
 
     const handleOpenEditModal = () => {
         setEditModalOpen(true);
@@ -36,8 +92,7 @@ export const UserProfile = () => {
 
     const handleSaveUser = async (editedUser: UserModel) => {
         setUser(editedUser);
-        const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4NSIsInBlcm1pc3Npb25zIjpbeyJhdXRob3JpdHkiOiJST0xFX1VOVkVSSUZJRUQifV0sImV4cCI6MTcwODIwNjQ5NCwiaWF0IjoxNzA3MzQyNDk0fQ.Qk83j3p8R3YNvuxIEor-RQflmsnBR3D9YJGde19p60o"; // Retrieve your token from storage
-
+        const token = localStorage.getItem("token")
         const url = 'http://localhost:8888/api/user/email'; // Replace with your actual endpoint
         const data = {
             email: editedUser.email // Make sure the email is correctly formatted
@@ -65,6 +120,68 @@ export const UserProfile = () => {
         }
     };
 
+    const handlerImageUpload = () => {
+        setOpenFileUpload(true)
+    }
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setFile(event.target.files[0]);
+        }
+    };
+
+    const handleLinkChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLink(event.target.value);
+    };
+
+
+    const handleUploadImageToBackend = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        console.log(file)
+        const apiEndpoint = 'http://localhost:8888/api/file/upload';
+
+        // Headers for fetch call
+        const headers = new Headers({
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        });
+
+        // Use FormData for file upload
+        let body: FormData | URLSearchParams;
+
+        if (file) { // If there's a file, append it to the FormData
+            body = new FormData();
+            body.append('file', file);
+        } else if (link) { // If there's a link, use URLSearchParams or a JSON payload
+            body = new URLSearchParams();
+            body.append('link', link);
+        } else {
+            console.error('No file or link to upload');
+            onCloseFileUpload()
+            return;
+        }
+
+        try {
+            const response = await fetch(apiEndpoint, {
+                method: 'POST',
+                headers: headers,
+                body: body,
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log(responseData.data);
+                window.alert(responseData.data)
+                onCloseFileUpload();
+                // Handle successful response
+            } else {
+                console.error('Upload failed:', response.statusText);
+                // Handle errors
+            }
+        } catch (error) {
+            console.error('Network error:', error);
+        }
+
+    }
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -86,11 +203,6 @@ export const UserProfile = () => {
                 // const result = JSON.parse(response.data)
                 const result = await response.json();
                 const a = result.data as UserModel;
-                // const convertData = {
-                //     ...a,
-                //     dateJoined: new Date(result.data.dateJoined.toLocaleDateString)
-                // }
-
                 setUser(a);
                 console.log(user)
             } catch (error) {
@@ -119,10 +231,7 @@ export const UserProfile = () => {
                 // const result = JSON.parse(response.data)
                 const result = await response.json();
                 const a = result.data as HistoryModel[]
-                console.log(a)
-
                 setHistory(a);
-                console.log(history)
             } catch (error) {
                 console.error("Fetching data failed:", error);
                 // Handle errors, such as by setting an error state (not shown here)
@@ -130,19 +239,80 @@ export const UserProfile = () => {
         }
 
         fetchUser();
+
         fetchHistory();
 
     }, [updateTrigger])
 
     const userInfoItems = (
-        <React.Fragment>
-            <Typography variant="h4">{user.firstName} {user.lastName}</Typography>
-            <Typography variant="body2">{user.email}</Typography>
-            <label> {showFormattedDate(user.dateJoined)}</label>
+        <Paper>
+            <Grid container spacing={2}>
+                <Grid container item xs={12}>
+                    <Grid item xs={5} />
+                    <Grid item xs={4}>
+                        <Box className="profile-image-overlay">
+                            <Avatar
+                                className="profile-image"
+                                src={user.profileImageURL}
+                            />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3} />
+                    <Grid item xs={5} />
+                    <Grid item xs={4}>
+                        <Button
+                            style={{ height: '1rem', width: "2rem", fontSize: "8pt" }}
+                            variant="contained"
+                            color="primary"
+                            onClick={handlerImageUpload}
+                        >
+                            Edit
+                        </Button>
+                    </Grid>
 
-            <Typography variant="body2">Role: {user.role}</Typography>
-            <Typography variant="body2">Status: {user.active ? 'Active' : 'Inactive'}</Typography>
-        </React.Fragment>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h6">{user.firstName}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h6"> {user.lastName}</Typography>
+                </Grid>
+                <Grid item xs={12} >
+                    <Typography variant="body1">{user.email}</Typography>
+                </Grid>
+
+                <Grid item container xs={12}>
+                    <Grid item xs={8}>
+                        <Typography variant="body2">Role: {user.role}</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        {user.role === 'UNVERIFIED' && (
+                            <Button
+                                style={{ height: '1rem', width: "2rem", fontSize: "8pt" }}
+                                variant="contained"
+                                color="primary"
+                                onClick={handleOpenVerifyDialog}
+                            // size="small"
+                            >
+                                Verify
+                            </Button>
+                        )}
+                    </Grid>
+
+
+                </Grid>
+                <Grid item xs={12}>
+                    <label>Member since: </label>
+                    <Typography variant="body2">{showFormattedDate(user.dateJoined)}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="body2">Status: {user.active ? 'Active' : 'Inactive'}</Typography>
+                </Grid>
+
+
+
+            </Grid>
+        </Paper >
     );
 
     // Generate list items for view history
@@ -163,28 +333,85 @@ export const UserProfile = () => {
 
     return (
 
-        <Grid sx={{ paddingTop: "2rem" }} container spacing={2}>
-            <Grid item xs={5}>
-                <Paper
-                    elevation={2}
-                >
+        <Grid sx={{ paddingTop: "3rem" }} container spacing={2}>
+
+            <Grid item xs={6}>
+                <Grid>
                     <Grid container spacing={1}>
-
-                        <Grid item xs={12} style={{ backgroundColor: "lightgrey" }}>
-                            <Avatar
-                                src={user.profileImageURL}
-                                sx={{ width: 100, height: 100 }}
-                            />
-                            <Grid>
-                                {userInfoItems}
-                            </Grid>
-
+                        <Grid item xs={12}>
+                            {userInfoItems}
                         </Grid>
                         <Grid item xs={12}>
-                            <Button
-                                variant="outlined"
-                                onClick={handleOpenEditModal}
-                            >Edit Profile</Button>
+                            <Dialog open={openFileUpload} onClose={onCloseFileUpload}>
+                                <DialogTitle>Upload File or Link</DialogTitle>
+                                <DialogContent>
+                                    <input
+                                        accept="*/*" // You can restrict file types by changing this string
+                                        id="file-upload"
+                                        type="file"
+                                        onChange={handleFileChange}
+                                        style={{ display: 'block', marginBottom: '20px' }}
+                                    />
+                                    <TextField
+                                        label="Or enter a link"
+                                        type="text"
+                                        fullWidth
+                                        variant="outlined"
+                                        value={link}
+                                        onChange={handleLinkChange}
+                                        disabled={!!file} // Disable if a file is selected
+                                    />
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button
+                                        onClick={onCloseFileUpload}
+                                    >Cancel</Button>
+                                    <Button
+                                        onClick={handleUploadImageToBackend}
+                                        color="primary"
+                                    >Upload
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Dialog open={openVerifyDialog} onClose={handleCloseVerifyDialog}>
+                                <DialogTitle>Verify User</DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText>
+                                        Please enter the verification code you received.
+                                    </DialogContentText>
+                                    <TextField
+                                        name="verifyCode"
+                                        autoFocus
+                                        margin="dense"
+                                        id="verificationCode"
+                                        label="Verification Code"
+                                        type="text"
+                                        fullWidth
+                                        variant="outlined"
+                                        value={verifyCode}
+                                        onChange={handlerVerifyCodeChange}
+                                    />
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleCloseVerifyDialog}>Cancel</Button>
+                                    <Button onClick={handleVerify}>Submit</Button>
+                                </DialogActions>
+                            </Dialog>
+                        </Grid>
+
+                        <Grid item container xs={12}>
+                            <Grid item xs={4} />
+                            <Grid item xs={4}>
+                                <Button
+                                    variant="outlined"
+                                    onClick={handleOpenEditModal}
+                                >Edit Profile</Button>
+                            </Grid>
+
                         </Grid>
                         <EditUserProfile
                             open={isEditModalOpen}
@@ -193,12 +420,15 @@ export const UserProfile = () => {
                             onSave={handleSaveUser}
                         />
                     </Grid>
-                </Paper>
-            </Grid>
-            <Grid item xs={7}>
+                </Grid>
+            </Grid >
+
+
+            <Grid item xs={6}>
                 {viewHistoryList}
             </Grid>
 
-        </Grid>
+
+        </Grid >
     );
 }
